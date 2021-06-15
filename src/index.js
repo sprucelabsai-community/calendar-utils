faf61ac699159bf07423109fa16ae06eb36530b2
aannaaD <anna.derkach@eliftech.com>
2021-06-15 15:46:24 +0000

    minor: added  calendarEventSchema export

--- src/index.js
+++
@@ -1,2 +0,0 @@
-export { default as calendarUtility } from './utilities/calendar.utility'
-export { default as dateUtility } from './utilities/date.utility'
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as calendarUtility } from './utilities/calendar.utility'
+export { default as dateUtility } from './utilities/date.utility'
+export { default as calendarEventSchema } from './.spruce/schemas/calendar/v2021_06_15/calendarEvent.schema'
+export { default as eventExclusionDateSchema } from './.spruce/schemas/calendar/v2021_06_15/eventExclusionDate.schema'

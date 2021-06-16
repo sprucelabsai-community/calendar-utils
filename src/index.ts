2f5681922af2fa24dbb9fa34fbb44b16adacb8f4
aannaaD <anna.derkach@eliftech.com>
2021-06-16 11:49:26 +0000

    minor: added duration utility

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as calendarUtility } from './utilities/calendar.utility'
 export { default as dateUtility } from './utilities/date.utility'
-export { default as calendarEventSchema } from './.spruce/schemas/calendar/v2021_06_15/calendarEvent.schema'
-export { default as eventExclusionDateSchema } from './.spruce/schemas/calendar/v2021_06_15/eventExclusionDate.schema'
+export { default as durationUtility } from './utilities/duration.utility'
+export { default as calendarEventSchema } from './.spruce/schemas/calendar/v2021_05_05/calendarEvent.schema'
+export { default as eventExclusionDateSchema } from './.spruce/schemas/calendar/v2021_05_05/eventExclusionDate.schema'
---
+++ src/utilities/duration.utility.ts
@@ -0,0 +1,27 @@
+const durationUtil = {
+	msToFriendly(duration: number): string {
+		let milliseconds = duration % 1000,
+			seconds = Math.floor((duration / 1000) % 60),
+			minutes = Math.floor((duration / (1000 * 60)) % 60),
+			hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+		let durationStr = ''
+		if (hours > 0) {
+			durationStr += `${hours}h `
+		}
+		if (hours > 0 || minutes > 0) {
+			durationStr += `${minutes}m `
+		}
+		if (durationStr.length > 0 || seconds > 0) {
+			durationStr += `${seconds}s `
+		}
+		if (hours === 0 && minutes === 0) {
+			if (seconds > 0) {
+				durationStr = `${seconds}.${milliseconds}s`
+			} else {
+				durationStr += `${milliseconds}ms`
+			}
+		}
+		return durationStr.trim()
+	},
+}
+export default durationUtil

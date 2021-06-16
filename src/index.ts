9a3420f3e04387c36c1ce7a339cc11b3f464d849
aannaaD <anna.derkach@eliftech.com>
2021-06-16 12:53:08 +0000

    major: changed exports name

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as calendarUtility } from './utilities/calendar.utility'
-export { default as dateUtility } from './utilities/date.utility'
-export { default as durationUtility } from './utilities/duration.utility'
+export { default as calendarUtil } from './utilities/calendar.utility'
+export { default as dateUtil } from './utilities/date.utility'
+export { default as durationUtil } from './utilities/duration.utility'
 export { default as calendarEventSchema } from './.spruce/schemas/calendar/v2021_05_19/calendarEvent.schema'
 export { default as eventExclusionDateSchema } from './.spruce/schemas/calendar/v2021_05_19/eventExclusionDate.schema'

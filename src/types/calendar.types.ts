40ba9c5be9d03a6a7b046a5ea42fbcb66e75bb2f
aannaaD <anna.derkach@eliftech.com>
2021-06-16 11:52:31 +0000

    patch: version fixes

--- src/types/calendar.types.ts
+++ src/types/calendar.types.ts
@@ -1,4 +1,4 @@
 import { SpruceSchemas } from '#spruce/schemas/schemas.types'
 
-export type CalendarEvent = SpruceSchemas.Calendar.v2021_06_15.CalendarEvent
+export type CalendarEvent = SpruceSchemas.Calendar.v2021_05_05.CalendarEvent
 export type DayOfWeek = NonNullable<CalendarEvent['daysOfWeek']>[number]

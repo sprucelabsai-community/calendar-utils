cef03dc5f26813e720023aa6421a06206caf196d
Ryan Johnson <ryan@sprucelabs.ai>
2022-01-13 09:14:09 -0500

    minor: people sorter utility can help sort people on a calendar based on name and selected events

---
+++ src/__tests__/behavioral/PeopleSorterUtil.test.ts
@@ -0,0 +1,294 @@
+import AbstractSpruceTest, { test, assert } from '@sprucelabs/test'
+import { PeopleSorter } from '../../utilities/peopleSorter.utility'
+
+export default class PeopleSorterUtilTest extends AbstractSpruceTest {
+	private static peopleSorterUtil: PeopleSorter
+	protected static async beforeEach() {
+		this.peopleSorterUtil = new PeopleSorter()
+		this.peopleSorterUtil.setEvents([])
+		this.peopleSorterUtil.setSelectedEvents([])
+		this.peopleSorterUtil.setPeople([])
+	}
+	@test()
+	protected static canCreatePeopleSorterUtility() {
+		assert.isTruthy(this.peopleSorterUtil)
+	}
+
+	@test()
+	protected static async hasSetPeopleMethod() {
+		assert.isFunction(this.peopleSorterUtil.setPeople)
+	}
+
+	@test()
+	protected static async hasGetPeopleMethod() {
+		assert.isFunction(this.peopleSorterUtil.getPeople)
+	}
+
+	@test()
+	protected static canProvidePeopleForSorting() {
+		const teammates = [{ id: '111', casualName: 'Test' }]
+		this.peopleSorterUtil.setPeople(teammates)
+
+		const people = this.peopleSorterUtil.getPeople()
+		assert.isLength(people, 1)
+		assert.isEqual(people?.[0]?.id, '111')
+	}
+
+	@test()
+	protected static async hasSortMethod() {
+		assert.isFunction(this.peopleSorterUtil.sort)
+	}
+
+	@test()
+	protected static async sortReturnPeopleForStart() {
+		const teammates = [
+			{ id: '111', casualName: 'ATest' },
+			{ id: '222', casualName: 'BTest' },
+		]
+
+		this.peopleSorterUtil.setPeople(teammates)
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 2)
+		assert.isEqual(sortedPeople?.[0]?.id, '111')
+	}
+
+	@test()
+	protected static async sortReturnsPeopleOrderedByCasualNameIfNoEventsProvided() {
+		const teammates = [
+			{ id: '111', casualName: 'B Test' },
+			{ id: '222', casualName: 'A Test' },
+		]
+
+		this.peopleSorterUtil.setPeople(teammates)
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 2)
+		assert.isEqual(sortedPeople?.[0]?.id, '222')
+	}
+
+	@test()
+	protected static async hasSetEventsMethod() {
+		assert.isFunction(this.peopleSorterUtil.setEvents)
+	}
+
+	@test()
+	protected static async hasGetEventsMethod() {
+		assert.isFunction(this.peopleSorterUtil.getEvents)
+	}
+
+	@test()
+	protected static canProvideEventsForSorting() {
+		this.peopleSorterUtil.setEvents([
+			{ id: '111', startDateTimeMs: 1641993534, personId: '222' },
+		])
+
+		const events = this.peopleSorterUtil.getEvents()
+		assert.isLength(events, 1)
+		assert.isEqual(events?.[0]?.id, '111')
+	}
+
+	@test()
+	protected static async sortingThrowsIfPeopleAreSetAndEventsSetWithoutSelectedEvents() {
+		this.peopleSorterUtil.setPeople([
+			{ id: '111', casualName: 'B Test' },
+			{ id: '222', casualName: 'A Test' },
+		])
+		this.peopleSorterUtil.setEvents([
+			{ id: '111', startDateTimeMs: 1641993534, personId: '111' },
+			{ id: '222', startDateTimeMs: 1641993848, personId: '222' },
+		])
+		await assert.doesThrowAsync(() => this.peopleSorterUtil.sort())
+	}
+
+	@test()
+	protected static async hasSetSelectedEventIdsMethod() {
+		assert.isFunction(this.peopleSorterUtil.setSelectedEvents)
+	}
+
+	@test()
+	protected static async hasGetSelectedEventIdsMethod() {
+		assert.isFunction(this.peopleSorterUtil.getSelectedEvents)
+	}
+
+	@test()
+	protected static canProvideSelectedEventsForSorting() {
+		this.peopleSorterUtil.setSelectedEvents([{ id: '111' }])
+
+		const events = this.peopleSorterUtil.getSelectedEvents()
+		assert.isLength(events, 1)
+		assert.isEqual(events?.[0]?.id, '111')
+	}
+
+	@test()
+	protected static async sortingThrowsIfPeopleSetAndSelectedEventsSetWithoutEvents() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'B Test' },
+			{ id: 'person222', casualName: 'A Test' },
+		])
+
+		this.peopleSorterUtil.setSelectedEvents([{ id: 'event111' }])
+
+		await assert.doesThrowAsync(() => this.peopleSorterUtil.sort())
+	}
+
+	@test()
+	protected static async canSortPeopleByEventStartDateIfSelectedEventsAreSet() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'B Test' },
+			{ id: 'person222', casualName: 'A Test' },
+		])
+
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event222', startDateTimeMs: 1641993848, personId: 'person222' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event222' },
+		])
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 2)
+		assert.isEqual(sortedPeople?.[0]?.id, 'person111')
+	}
+
+	@test()
+	protected static async canSortPeopleByEventStartDateIfSelectedEventsAreSetForTwoPersonOutOfTree() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'C Test' },
+			{ id: 'person222', casualName: 'B Test' },
+			{ id: 'person333', casualName: 'A Test' },
+		])
+
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event222', startDateTimeMs: 1641993848, personId: 'person222' },
+			{ id: 'event333', startDateTimeMs: 1641993833, personId: 'person333' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event222' },
+		])
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 3)
+		assert.isEqual(sortedPeople?.[0]?.id, 'person111')
+	}
+
+	@test()
+	protected static async cantSortIfPeopleAreNotSet() {
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event222', startDateTimeMs: 1641993848, personId: 'person222' },
+			{ id: 'event333', startDateTimeMs: 1641993833, personId: 'person333' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event222' },
+		])
+		await assert.doesThrowAsync(() => this.peopleSorterUtil.sort())
+	}
+
+	@test()
+	protected static async cantSortIfPersonIsMissgingInEventsList() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'C Test' },
+			{ id: 'person222', casualName: 'B Test' },
+			{ id: 'person333', casualName: 'A Test' },
+		])
+
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event222', startDateTimeMs: 1641993848, personId: 'person222' },
+			{ id: 'event444', startDateTimeMs: 1641993833, personId: 'person444' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event444' },
+			{ id: 'event222' },
+		])
+		await assert.doesThrowAsync(() => this.peopleSorterUtil.sort())
+	}
+
+	@test()
+	protected static async canSortByEventStartDateAndPeopleCasualName() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'C Test' },
+			{ id: 'person222', casualName: 'K Test' },
+			{ id: 'person333', casualName: 'A Test' },
+			{ id: 'person444', casualName: 'B Test' },
+		])
+
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event333', startDateTimeMs: 1641993839, personId: 'person333' },
+			{ id: 'event444', startDateTimeMs: 1641993831, personId: 'person444' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event333' },
+		])
+
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 4)
+		assert.isEqual(sortedPeople?.[0]?.casualName, 'C Test')
+		assert.isEqual(sortedPeople?.[1]?.casualName, 'A Test')
+		assert.isEqual(sortedPeople?.[2]?.casualName, 'B Test')
+		assert.isEqual(sortedPeople?.[3]?.casualName, 'K Test')
+	}
+
+	@test()
+	protected static async canSortByEventStartDate() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'C Test' },
+			{ id: 'person222', casualName: 'K Test' },
+			{ id: 'person333', casualName: 'A Test' },
+			{ id: 'person444', casualName: 'B Test' },
+		])
+
+		this.peopleSorterUtil.setEvents([
+			{ id: 'event111', startDateTimeMs: 1641993534, personId: 'person111' },
+			{ id: 'event222', startDateTimeMs: 1641993840, personId: 'person222' },
+			{ id: 'event333', startDateTimeMs: 1641993839, personId: 'person333' },
+			{ id: 'event444', startDateTimeMs: 1641993831, personId: 'person444' },
+		])
+		this.peopleSorterUtil.setSelectedEvents([
+			{ id: 'event111' },
+			{ id: 'event333' },
+			{ id: 'event222' },
+			{ id: 'event444' },
+		])
+
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 4)
+		assert.isEqual(sortedPeople?.[0]?.casualName, 'C Test')
+		assert.isEqual(sortedPeople?.[1]?.casualName, 'B Test')
+		assert.isEqual(sortedPeople?.[2]?.casualName, 'A Test')
+		assert.isEqual(sortedPeople?.[3]?.casualName, 'K Test')
+	}
+
+	@test()
+	protected static async canClearSelectedEvents() {
+		this.peopleSorterUtil.setPeople([
+			{ id: 'person111', casualName: 'C Test' },
+			{ id: 'person222', casualName: 'K Test' },
+			{ id: 'person333', casualName: 'A Test' },
+			{ id: 'person444', casualName: 'B Test' },
+		])
+
+		this.peopleSorterUtil.setSelectedEvents([])
+		const selectedEvents = this.peopleSorterUtil.getSelectedEvents()
+		assert.isLength(selectedEvents, 0)
+
+		const sortedPeople = this.peopleSorterUtil.sort()
+
+		assert.isLength(sortedPeople, 4)
+		assert.isEqual(sortedPeople?.[0]?.casualName, 'A Test')
+		assert.isEqual(sortedPeople?.[1]?.casualName, 'B Test')
+		assert.isEqual(sortedPeople?.[2]?.casualName, 'C Test')
+		assert.isEqual(sortedPeople?.[3]?.casualName, 'K Test')
+	}
+}
---
+++ src/utilities/peopleSorter.utility.ts
@@ -0,0 +1,142 @@
+import { sortBy } from 'lodash'
+
+export type Person = { id: string; casualName: string }
+export type CalendarEvent = {
+	id: string
+	startDateTimeMs: number
+	personId: string
+}
+export type SelectedEvent = Pick<CalendarEvent, 'id'>
+
+export default class PeopleSorter {
+	private people: Person[] = []
+	private events: CalendarEvent[] = []
+	private selectedEvents: SelectedEvent[] = []
+
+	public setPeople(people: Person[]) {
+		this.people = [...people]
+	}
+	public getPeople() {
+		return this.people
+	}
+
+	public sort() {
+		this.assertValid()
+
+		let peopleSortResult: Person[] = []
+		if (this.selectedEvents.length > 0 && this.events.length > 0) {
+			let selectedEventsWithValidPeople = this.events.filter((e) => {
+				const eventsMatch = this.selectedEvents.find((se) => se.id === e.id)
+				const peopleMatch = this.people.find((p) => p.id === e.personId)
+				if (peopleMatch && eventsMatch) {
+					return true
+				}
+				return false
+			})
+			selectedEventsWithValidPeople = sortBy(selectedEventsWithValidPeople, [
+				'startDateTimeMs',
+			])
+			const selectedPeopleWithValidEvents = selectedEventsWithValidPeople.map(
+				(e) => {
+					const match = this.people.find((p) => p.id === e.personId)
+					if (match) {
+						return match
+					}
+					return null
+				}
+			) as Person[]
+
+			const nonSelectedPeople = this.people.filter((p) => {
+				const match = selectedPeopleWithValidEvents.find(
+					(sp) => sp?.id === p.id
+				)
+				if (match) {
+					return false
+				}
+				return true
+			})
+			if (
+				selectedPeopleWithValidEvents &&
+				selectedPeopleWithValidEvents.length > 0
+			) {
+				peopleSortResult = selectedPeopleWithValidEvents
+			}
+
+			if (nonSelectedPeople && nonSelectedPeople.length > 0) {
+				peopleSortResult = peopleSortResult.concat(
+					sortBy(nonSelectedPeople, ['casualName'])
+				)
+			}
+		} else {
+			peopleSortResult = sortBy(this.people, ['casualName'])
+		}
+
+		return peopleSortResult
+	}
+
+	public setEvents(events: CalendarEvent[]) {
+		this.events = [...events]
+	}
+
+	public getEvents() {
+		return this.events
+	}
+
+	public setSelectedEvents(selectedEvents: Pick<CalendarEvent, 'id'>[]) {
+		this.selectedEvents = [...selectedEvents]
+	}
+
+	public getSelectedEvents() {
+		return this.selectedEvents
+	}
+
+	private assertValid() {
+		if (!this.people || this.people.length == 0) {
+			throw new Error('People are not set for sorting')
+		}
+
+		if (this.events.length > 0 && this.selectedEvents.length == 0) {
+			throw new Error('Selected events are not set for sorting')
+		}
+
+		if (this.selectedEvents.length > 0 && this.events.length == 0) {
+			throw new Error('Events are not set for sorting')
+		}
+		this.assertEventsAreValid()
+		this.assertPeopleAreValid()
+	}
+
+	private assertEventsAreValid() {
+		const eventIds = this.events.map((x) => x.id)
+
+		const missingSelectedEventIds = this.selectedEvents
+			.filter((k) => eventIds.indexOf(k.id) === -1)
+			.map((e) => e.id)
+
+		if (missingSelectedEventIds.length > 0) {
+			throw new Error(
+				`Selected event ids are not present in events list: [${missingSelectedEventIds.join(
+					', '
+				)}]`
+			)
+		}
+	}
+
+	private assertPeopleAreValid() {
+		const eventsPersonIds = this.events.map((x) => x.personId)
+
+		const personIds = this.people.map((x) => x.id)
+
+		const missingPersonIds = eventsPersonIds.filter(
+			(k) => personIds.indexOf(k) === -1
+		)
+
+		if (missingPersonIds.length > 0) {
+			throw new Error(
+				`Event person are not present in people list: [${missingPersonIds.join(
+					', '
+				)}]`
+			)
+		}
+	}
+}
